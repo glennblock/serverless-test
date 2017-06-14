92a7fd8105e9506fbe33f0edb77562d01142ac23
Glenn Block <glenn.block@gmail.com>
2017-06-14 11:47:40 -0700

    Create serverless-test.js

--- serverless-test.js
+++ serverless-test.js
@@ -1,5 +1,5 @@
 module.exports = function(ctx, cb) {
   var twilio = require('twilio');
-  
+  console.log("Look ma I edited my task from Github");
   cb(null, { hello: ctx.data.name || 'Anonymous' });
 };
